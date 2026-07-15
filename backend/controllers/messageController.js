772ba82e699720dc4c043e983d4e40f6be4d44b2
Anmolkr007 <anmolchaubey777@gmail.com>
2026-07-16 04:51:56 +0530

    unblock fixed

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -222,7 +222,7 @@ export const handleFriendRequest = async (req, res) => {
                     return res.status(200).json({
                         success: true,
                         message: "User unblocked",
-                        relationshipStatus: "none"
+                        relationshipStatus: "accepted"
                     });
                 }
 
